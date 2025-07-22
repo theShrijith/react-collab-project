f2d3339be558628b89a505b319f012b82b08d3df
theShrijith <shrijith1224@gmail.com>
2025-07-22 17:34:31 +0530

    Update logo and link to Propeller Aero

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to My React App!</h1>
         <p>This is a new header message. Enjoy exploring React!</p>
+        <a
+          className="App-link"
+          href="https://www.propelleraero.com/dirtmate/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more
+        </a>
       </header>
     </div>
   );
